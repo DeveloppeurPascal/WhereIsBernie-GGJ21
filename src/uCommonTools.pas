08b5b9cff86896220b3634ff7c280569cc06d862
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-11-01 11:27:38 +0100

    added header copyright and timestamp comments

--- src/uCommonTools.pas
+++ src/uCommonTools.pas
@@ -1,3 +1,35 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Where is Bernie ?
+
+  Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://whereisbernie.gamolf.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/WhereIsBernie-GGJ21
+
+  ***************************************************************************
+  File last update : 2025-11-01T11:21:14.000+01:00
+  Signature : 8b97d65798d3d6a4f863adf007887a5c46a50b4c
+  ***************************************************************************
+*)
+
 unit uCommonTools;
 
 interface
--- src/uScores.pas
+++ src/uScores.pas
@@ -1,3 +1,35 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Where is Bernie ?
+
+  Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://whereisbernie.gamolf.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/WhereIsBernie-GGJ21
+
+  ***************************************************************************
+  File last update : 2025-10-28T20:00:02.956+01:00
+  Signature : 852b424f7059fac38493c1b6f6b743fc63164a4a
+  ***************************************************************************
+*)
+
 unit uScores;
 
 interface
